363d0780c5bb2ccf88ff389caa990aef9d31d154
Stanislav Vassiljev <stanvas@gmail.com>
2023-12-18 19:42:38 +0200

    added startup logs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import { app } from "./app";
 import { DatabaseConnectionError } from "@stantickets/common";
 
 const startUp = async () => {
+  console.log("Starting up...");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
